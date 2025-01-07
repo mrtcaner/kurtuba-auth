8845ef5723ef2a19f01eafb07d722df0e5582d66
murat <muratcaner@ymail.com>
2025-01-07 13:09:17 +0300

    - UserMetaChange repository and service methods now return Optional<> results - Code cleanup, refactor, bugfixes

--- src/main/java/com/kurtuba/auth/data/repository/UserMetaChangeRepository.java
+++ src/main/java/com/kurtuba/auth/data/repository/UserMetaChangeRepository.java
@@ -4,18 +4,19 @@ import com.kurtuba.auth.data.enums.MetaOperationType;
 import com.kurtuba.auth.data.model.UserMetaChange;
 import org.springframework.data.repository.CrudRepository;
 ;import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface UserMetaChangeRepository extends CrudRepository<UserMetaChange, String> {
 
-    UserMetaChange findByLinkParam(String linkParam);
+    Optional<UserMetaChange> findByLinkParam(String linkParam);
 
     void deleteAllByExecutedIsFalseAndUserIdAndMetaOperationType(String userId, MetaOperationType metaOperationType);
 
-    UserMetaChange findByUserIdAndMetaOperationTypeAndExpirationDateAfterAndExecutedIsFalse(String userId,
+    Optional<UserMetaChange> findByUserIdAndMetaOperationTypeAndExpirationDateAfterAndExecutedIsFalse(String userId,
                                                                                             MetaOperationType metaOperationType,
                                                                                             LocalDateTime after);
 
-    UserMetaChange findByLinkParamAndMetaOperationTypeAndExpirationDateAfterAndExecutedIsFalse(String linkParam,
+    Optional<UserMetaChange> findByLinkParamAndMetaOperationTypeAndExpirationDateAfterAndExecutedIsFalse(String linkParam,
                                                                                                MetaOperationType metaOperationType,
                                                                                                LocalDateTime after);
 
